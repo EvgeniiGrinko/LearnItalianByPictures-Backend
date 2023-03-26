10c886a1afec7dcc864eb0afe0aed85237ee0597
EvgeniiGrinko <jenek0907@mail.ru>
2023-03-26 22:07:15 +0500

    changes in logging errors

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -12,31 +12,43 @@ class UserController extends Controller
 {
     public function login()
     {
-        $requestData = request()->input();
-        $user = User::where("login", $requestData["login"])->first();
-        Log::info("Request register new user data", [
-            "requestData" => $requestData, "user" => $user
-        ]);
-        if ($user == null) {
-            Log::info("error occurred while logging in user ");
-            return response()->json([
-                "status" => "not ok"
-            ], 400);
-        } elseif ($user->password == sha1($requestData["password"])) {
-            $res = response()->json([
-                "status" => "ok",
-                "id" => $user->id,
-                "name" => $user->id,
-                "surname" => $user->surname,
-                "login" => $user->login,
+        try {
+            $requestData = request()->input();
+            $user = User::where("login", $requestData["login"])->first();
+            Log::channel("stack")->info("Request register new user data", [
+                "requestData" => $requestData, "user" => $user
             ]);
-            Log::info("response register new user data", [
-                "response" => $res
-            ]);
-            return $res;
+            if ($user == null) {
+                Log::channel("stack")->error("error occurred while logging in user ");
+                return response()->json([
+                    "status" => "not ok"
+                ], 400);
+            } elseif ($user->password == sha1($requestData["password"])) {
+                $res = response()->json([
+                    "status" => "ok",
+                    "id" => $user->id,
+                    "name" => $user->id,
+                    "surname" => $user->surname,
+                    "login" => $user->login,
+                ]);
+                Log::channel("stack")->info("response register new user data", [
+                    "response" => $res
+                ]);
+                return $res;
 
-        } else{
-            Log::info("e111rror occurred while logging in user ", [$user->password === Hash::make($requestData["password"])]);
+            } else {
+                Log::channel("stack")->error("e111rror occurred while logging in user ", [$user->password === Hash::make($requestData["password"])]);
+                return response()->json([
+                    "status" => "not ok"
+                ], 400);
+            }
+        } catch (\Throwable $t){
+            Log::channel("stack")->error("Exception occurred while logging in user ", [
+                "code" => $t->getCode(),
+                "message" => $t->getMessage(),
+                "line" => $t->getLine(),
+                "file" => $t->getFile(),
+            ]);
             return response()->json([
                 "status" => "not ok"
             ], 400);
@@ -55,11 +67,11 @@ class UserController extends Controller
                 "login" => $requestData["login"],
                 "password" => sha1($requestData["password"]),
             ]);
-            Log::info("Request register new user data", [
+            Log::channel("stack")->info("Request register new user data", [
                 "requestData" => $requestData, "user" => $user
             ]);
         } catch (Exception $t) {
-            Log::error("Error occurred while creating new User", [
+            Log::channel("stack")->error("Error occurred while creating new User", [
                 "code" => $t->getCode(),
                 "message" => $t->getMessage(),
                 "line" => $t->getLine(),
